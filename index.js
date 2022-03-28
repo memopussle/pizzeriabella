190d3854c9619794b64eab60d2fb76cda04c7641
thugiang <xanhtham.cuc@gmail.com>
2022-03-28 19:59:59 +1300

    hamburger

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 //HAMBURGER
 window.onload = function () {
+  //choose hamburger class
+  const menuBtn = document.querySelector(".hamburger");
 
-    //choose hamburger class
-    const menuBtn = document.querySelector('.hamburger');
-
-    //add event listener when user click menu btn
-    menuBtn.addEventListener('click', function () {
-      menuBtn.classList.add('is-active');
-    });
-}
+  //choose hamburger nav
+  const mobileMenu = document.querySelector(".hamburger__nav");
+  
+  //add event listener when user click menu btn
+  //when click menu btn, it will either add or remove class is-active
+  menuBtn.addEventListener("click", function () {
+    menuBtn.classList.toggle("is-active");
+    mobileMenu.classList.toggle("is-active");
+  });
+};
