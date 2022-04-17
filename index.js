78a57fc160eaeaf97dce8ebea87fb95d1709a41f
thugiang <xanhtham.cuc@gmail.com>
2022-04-18 01:40:08 +1200

    .

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ window.onload = function () {
   //choose hamburger nav
   const mobileMenu = document.querySelector(".hamburger__nav");
   
+  //hidie header text-box
+  const headerText = document.querySelector(".header__text-box");
+
   //add event listener when user click menu btn
   //when click menu btn, it will either add or remove class is-active
   menuBtn.addEventListener("click", function () {
     menuBtn.classList.toggle("is-active");
     mobileMenu.classList.toggle("is-active");
+    headerText.classList.toggle("is-active");
   });
 };
